77fea742ef861627d955e05ac02e55f653e0aafe
cpz-baron <57302955+cpz-baron@users.noreply.github.com>
2020-01-24 12:34:36 -0800

    Week 3 exercises added

--- exercise.R
+++ exercise.R
@@ -1,65 +1,51 @@
-# Exercise 2: working with data frames
+# Exercise 1: creating and accessing lists
 
-# Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
-# Hint: use the `paste()` function and vector recycling to add a number to the word
-# "Employee"
-employees <- paste("Enmployee", 1:100)
-print(employees)
+# Create a vector `my_breakfast` of everything you ate for breakfast
+my_breakfast <- c("bread", "noodles", "doupi")
 
-# Create a vector of 100 random salaries for the year 2017
-# Use the `runif()` function to pick random numbers between 40000 and 50000
-salaries_2017 <- runif(100,40000, 50000) 
+# Create a vector `my_lunch` of everything you ate (or will eat) for lunch
+my_lunch <- c("rice", "noodles", "dumplings")
 
-# Create a vector of 100 annual salary adjustments between -5000 and 10000.
-# (A negative number represents a salary decrease due to corporate greed)
-# Again use the `runif()` function to pick 100 random numbers in that range.
-adjustments <- runif(100, -5000, 10000)
+# Create a list `meals` that has contains your breakfast and lunch
+meals <- list(breakfast = my_breakfast, lunch = my_lunch)
+print(meals)
 
-# Create a data frame `salaries` by combining the 3 vectors you just made
-# Remember to set `stringsAsFactors=FALSE`!
-salaries <- data.frame(employees, salaries_2017, adjustments, stringsAsFactors = FALSE)
+# Add a "dinner" element to your `meals` list that has what you plan to eat 
+# for dinner
+meals$dinner <- c("steak", "7 up")
+print(meals)
 
-# Add a column to the `salaries` data frame that represents each person's
-# salary in 2018 (e.g., with the salary adjustment added in).
-salaries$salaries_2018 <- salaries$salaries_2017 + salaries$adjusments
-View(salaries)
+# Use dollar notation to extract your `dinner` element from your list
+# and save it in a vector called 'dinner'
+dinner <- meals$dinner
+print(dinner)
 
-# Add a column to the `salaries` data frame that has a value of `TRUE` if the 
-# person got a raise (their salary went up)
-salaries$got_raise <- salaries$adjustments > 0
+# Use double-bracket notation to extract your `lunch` element from your list
+# and save it in your list as the element at index 5 (no reason beyond practice)
+meals[[5]] <- meals[["lunch"]]
+print(meals)
 
+# Use single-bracket notation to extract your breakfast and lunch from your list
+# and save them to a list called `early_meals`
+early_meals <- meals[c("breakfast", "lunch")]
+print(early_meals)
 
-### Retrieve values from your data frame to answer the following questions
-### Note that you should get the value as specific as possible (e.g., a single
-### cell rather than the whole row!)
+### Challenge ###
 
-# What was the 2018 salary of Employee 57
-salaries[salaries$employees == "Employee 57", "salaries_2018"]
+# Create a list that has the number of items you ate for each meal
+# Hint: use the `lappy()` function to apply the `length()` function to each item
+num_of_breakfast <- length(meals$breakfast)
+num_items <- lapply(meals, length)
+print(num_items)
 
-# How many employees got a raise?
-nrow(salaries[salaries$got_raise == TRUE, ])
+# Write a function `add_pizza` that adds pizza to a given meal vector, and
+# returns the pizza-fied vector
+add_pizza <- function(meal_vector) {
+  new_vector <- c(meal_vector, "pizza")
+  return(new_vector)
+}
 
-# What was the dollar value of the highest raise?
-salaries[salaries$adjustments == max(salaries$adjustments), "adjustments"]
+add_pizza(c("nachos"))
 
-# What was the "name" of the employee who received the highest raise?
-salaries[salaries$adjustments == max(salaries$adjustments), "employees"]
-
-# What was the largest decrease in salaries between the two years?
-
-
-# What was the name of the employee who recieved largest decrease in salary?
-
-
-# What was the average salary change?
-
-
-# For people who did not get a raise, how much money did they lose on average?
-paycut <- salaries[salaries$got_raise == FALSE, ]
-mean(paycut$adjustments)
-
-## Consider: do the above averages match what you expected them to be based on 
-## how you generated the salaries?
-
-# Write a .csv file of your salary data to your working directory
-
+# Create a vector `better_meals` that is all your meals, but with pizza!
+lapply(meals, add_pizza)
